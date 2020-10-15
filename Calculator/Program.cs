f060d542289456781ce7a1a14f083fd26ec83bde
Angstsh <1305065108@qq.com>
2020-10-15 10:45:36 +0800

    计算器2.0 代码规范

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,22 +6,40 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.Write("请输入数字A：");
-            string A = Console.ReadLine();
-            Console.Write("请输入运算符号（+、-、*、/）：");
-            string B = Console.ReadLine();
-            Console.Write("请输入数字B：");
-            string C = Console.ReadLine();
-            string D = "";
-            if (B == "+")
-                D = Convert.ToString(Convert.ToDouble(A)+Convert.ToDouble(C));
-            if (B == "-")
-                D = Convert.ToString(Convert.ToDouble(A) - Convert.ToDouble(C));
-            if (B == "*")
-                D = Convert.ToString(Convert.ToDouble(A) * Convert.ToDouble(C));
-            if (B == "/")
-                D = Convert.ToString(Convert.ToDouble(A) / Convert.ToDouble(C));
-            Console.WriteLine("结果是："+D);
+            try
+            {
+                Console.Write("请输入数字A：");
+                string strNumberA = Console.ReadLine();
+                Console.Write("请输入运算符号（+、-、*、/）：");
+                string strOperate = Console.ReadLine();
+                Console.Write("请输入数字B：");
+                string strNumberB = Console.ReadLine();
+                string strResult = "";
+                switch (strOperate)
+                {
+                    case "+":
+                        strResult = Convert.ToString(Convert.ToDouble(strNumberA) + Convert.ToDouble(strNumberB));
+                        break;
+                    case "-":
+                        strResult = Convert.ToString(Convert.ToDouble(strNumberA) - Convert.ToDouble(strNumberB));
+                        break;
+                    case "*":
+                        strResult = Convert.ToString(Convert.ToDouble(strNumberA) * Convert.ToDouble(strNumberB));
+                        break;
+                    case "/":
+                        if (strNumberB != "0")
+                            strResult = Convert.ToString(Convert.ToDouble(strNumberA) / Convert.ToDouble(strNumberB));
+                        else
+                            strResult = "除数不能为0";
+                        break;
+                }
+                Console.WriteLine("结果是：" + strResult);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("您的输入有错：" + ex.Message);
+            }
         }
     }
 }
