fe94cda4e7ba4d65aea8f0c8160e45c08b13b30b
Angstsh <1305065108@qq.com>
2020-10-15 11:51:23 +0800

    计算器3.0 面向对象--业务代码封装

---
+++ Calculator/Operation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    /*运算业务类*/
+    public class Operation
+    {
+        public static double GetResult(double numberA, double numberB, string operate)
+        {
+            double result = 0d;
+            switch (operate)
+            {
+                case "+":
+                    result = numberA + numberB;
+                    break;
+                case "-":
+                    result = numberA - numberB;
+                    break;
+                case "*":
+                    result = numberA * numberB;
+                    break;
+                case "/":
+                    result = numberA / numberB;
+                    break;
+
+            }
+            return result;
+        }
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -15,24 +15,7 @@ namespace Calculator
                 Console.Write("请输入数字B：");
                 string strNumberB = Console.ReadLine();
                 string strResult = "";
-                switch (strOperate)
-                {
-                    case "+":
-                        strResult = Convert.ToString(Convert.ToDouble(strNumberA) + Convert.ToDouble(strNumberB));
-                        break;
-                    case "-":
-                        strResult = Convert.ToString(Convert.ToDouble(strNumberA) - Convert.ToDouble(strNumberB));
-                        break;
-                    case "*":
-                        strResult = Convert.ToString(Convert.ToDouble(strNumberA) * Convert.ToDouble(strNumberB));
-                        break;
-                    case "/":
-                        if (strNumberB != "0")
-                            strResult = Convert.ToString(Convert.ToDouble(strNumberA) / Convert.ToDouble(strNumberB));
-                        else
-                            strResult = "除数不能为0";
-                        break;
-                }
+                strResult = Convert.ToString(Operation.GetResult(Convert.ToDouble(strNumberA),Convert.ToDouble(strNumberB),strOperate));
                 Console.WriteLine("结果是：" + strResult);
                 Console.ReadLine();
             }
